4b02f137b5af7fdf9369510110f46e3757496eac
rm86073 <rm86073@BF-P4L1-M30.fieb.local>
2023-06-01 11:07:33 -0300

    Início controle de versão

---
+++ src/main/java/com/itb/inf2cm/comercio/mode/Produto.java
@@ -0,0 +1,14 @@
+package com.itb.inf2cm.comercio.mode;
+
+public class Produto {
+	
+	private Long id;
+	private String nome;
+	private String codigoDeBarras;
+	private double preco;
+	
+	// public : Acesso liberado para todas as classes
+	// private : Acesso liberado apenas para os membros da própria
+	//			classe, ou seja só é acessado dentro da classe.
+	
+}
